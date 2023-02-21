7e20b807643595e25b349c857d4dd553875b6e2b
David Garcia <87497958+Dgarc359@users.noreply.github.com>
2023-02-20 22:23:08 -0500

    v0.1.3

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,21 @@
-export class DiscordAuthorizer {
-  constructor() {
-    console.debug('Hello world');
-  }
+import * as nacl from 'tweetnacl';
+
+interface IHeaders {
+  [name: string]: string | undefined;
 }
+
+export const verifyDiscordRequest = (body: string, headers: IHeaders, botPublicKey: string): boolean => {
+  const signature = headers['x-signature-ed25519'];
+  const timestamp = headers['x-signature-timestamp'];
+
+  if (!signature) throw new Error('No signature found in headers');
+  if (!timestamp) throw new Error('No timestamp found in headers');
+
+  const isVerified = nacl.sign.detached.verify(
+    Buffer.from(timestamp + body),
+    Buffer.from(signature, 'hex'),
+    Buffer.from(botPublicKey, 'hex'),
+  );
+
+  return isVerified;
+};
